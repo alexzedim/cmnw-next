1ec24464f77e6bfc4f38bf915e0ca868c9b39bab
Sasha <alexzedim@gmail.com>
2025-12-18 23:00:49 +0300

    fix geist font css variables

--- config/fonts.ts
+++ config/fonts.ts
@@ -2,6 +2,4 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 
 // Geist package includes full glyph set with Cyrillic support
-export const fontSans = GeistSans;
-
-export const fontMono = GeistMono;
+export { GeistSans as fontSans, GeistMono as fontMono };
