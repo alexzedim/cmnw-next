7ccd842e651d01f8389a2ea3a605a7f595badb7e
Sasha <alexzedim@gmail.com>
2026-01-01 16:14:18 +0300

    chore(deps): update baseline-browser-mapping to version 2.9.11 and set package manager to pnpm@10.27.0

--- utils/random.ts
+++ utils/random.ts
@@ -1,11 +1,13 @@
 export function getRandomItems<T>(array: T[], count: number): string {
   const randomValues = new Uint32Array(count);
+
   crypto.getRandomValues(randomValues);
 
-  let result = '';
+  let result = "";
 
   for (let i = 0; i < count; i++) {
     const randomIndex = randomValues[i] % array.length;
+
     result += array[randomIndex];
   }
 
