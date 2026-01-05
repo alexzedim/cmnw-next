ef7a92ff8f11208335e4910552a51027bfb038f3
Sasha <alexzedim@gmail.com>
2026-01-06 01:09:41 +0300

    build(config): update api origin settings

--- config/api-origin.js
+++ config/api-origin.js
@@ -1,6 +1,6 @@
 const normalizeOrigin = (origin) => (origin ?? "").replace(/\/+$/, "");
 
-const DEFAULT_API_ORIGIN = normalizeOrigin("https://cmnw.me");
+const DEFAULT_API_ORIGIN = normalizeOrigin("http://128.0.0.255:8080");
 const API_ORIGIN = DEFAULT_API_ORIGIN;
 
 module.exports = {
