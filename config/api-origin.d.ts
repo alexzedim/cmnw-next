5fc62cd2e1033b882d465dbde2e799471552a4ec
Sasha <alexzedim@gmail.com>
2026-01-06 01:27:31 +0300

    chore(config): update api origin definitions

--- config/api-origin.d.ts
+++ config/api-origin.d.ts
@@ -1,3 +1,4 @@
 export declare const normalizeOrigin: (origin?: string) => string;
-export declare const DEFAULT_API_ORIGIN: string;
+export declare const INTERNAL_API_ORIGIN: string;
+export declare const PUBLIC_API_ORIGIN: string;
 export declare const API_ORIGIN: string;
