c9388e7e5a08e5d18a2b4b3629e110fd5fecf773
Sasha <alexzedim@gmail.com>
2026-04-09 15:43:10 +0300

    style(css): replace hardcoded orange-500 with CSS variables

--- config/api-origin.js
+++ config/api-origin.js
@@ -1,11 +1,17 @@
 const normalizeOrigin = (origin) => (origin ?? "").replace(/\/+$/, "");
 
+const envApiUrl = normalizeOrigin(process.env.API_URL);
+
 const INTERNAL_API_ORIGIN = normalizeOrigin("http://128.0.0.255:8080");
 const PUBLIC_API_ORIGIN = normalizeOrigin("https://cmnw.me");
 
 const isServerRuntime = () => typeof window === "undefined";
 
-const API_ORIGIN = isServerRuntime() ? INTERNAL_API_ORIGIN : PUBLIC_API_ORIGIN;
+const API_ORIGIN = envApiUrl
+  ? envApiUrl
+  : isServerRuntime()
+    ? INTERNAL_API_ORIGIN
+    : PUBLIC_API_ORIGIN;
 
 module.exports = {
   API_ORIGIN,
