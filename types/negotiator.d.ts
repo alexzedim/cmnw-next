48f66ce0e64e17689deba041c1ce5cfbd571fbf6
Sasha <alexzedim@gmail.com>
2026-04-30 08:55:07 +0300

    chore(types): remove obsolete type files and update tsconfig typeroots

--- types/negotiator.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "negotiator" {
-  type Headers = Record<string, string>;
-  class Negotiator {
-    constructor(request: { headers: Headers });
-    languages(): string[];
-  }
-  export = Negotiator;
-}
